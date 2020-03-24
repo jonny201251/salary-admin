5699350880b6774914828c9a06b785025aa2a6e7
jonny201251 <jonny201251@163.com>
2020-03-24 10:30:01 +0800

    20200323部署前的修改

--- src/main/java/com/hthyaq/salaryadmin/controller/IndexController.java
+++ src/main/java/com/hthyaq/salaryadmin/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping(value = {"/login", "/dept", "/dic", "/user", "/salNp", "/salLtx", "/salLx", "/changeSheet", "/changeSheetNeedHandle", "/permission", "/role", "/main", "/userSalNp"})
+    @GetMapping(value = {"/","/login", "/dept", "/dic", "/user", "/salNp", "/salLtx", "/salLx", "/changeSheet", "/changeSheetNeedHandle", "/permission", "/role", "/main", "/userSalNp"})
     public String index() {
         return "index";
     }
